b3d611ac456cdf586a44107e82aa53a0ea23ebf5
nadws <nandw567@gmail.com>
2023-02-27 14:55:51 +0800

    first

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@moonton.tes',
+            'password' => bcrypt('password')
+        ]);
+    }
+}
